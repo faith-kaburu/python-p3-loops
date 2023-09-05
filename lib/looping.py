b8715b173ce61c13a1c2551402d967acc69ab1ea
faith kaburu <faith.kaburu@student.moringaschool.com>
2023-09-05 23:46:35 +0300

    initial commit

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    
+    while count >= 1:
+        print(count)
+        count -= 1
+    
+    print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+happy_new_year()
+
+def square_integers(num_list):
+    squared_nums = [num ** 2 for num in num_list]
+    return squared_nums
+
+input_list = [1, 2, 3, 4, 5]
+squared_list = square_integers(input_list)
+print(squared_list)
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+# Call the fizzbuzz function
+fizzbuzz()
